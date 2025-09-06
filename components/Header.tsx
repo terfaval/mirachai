8de326745f1dc8aa2f440db9a49ba017887e661e
terfaval <mate.fater@gmail.com>
2025-09-06 13:44:07 +0200

    fixed header new backgrounds

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,12 +13,20 @@ export default function Header({ query, onChange }: Props) {
       <div className={styles.actions}>
         <SearchBar query={query} onChange={onChange} />
         <div className={styles.iconContainer}>
-          <img src="/search.svg" className={`search-button ${styles.icon}`} alt="Keresés" />
+          <img
+            src="/search.svg"
+            className={`search-button ${styles.icon}`}
+            alt="Keresés"
+          />
           <span className={styles.tooltip}>Keresés</span>
-        <div className={styles.iconContainer}>
-          <img src="/filter.svg" className={`filter-button ${styles.icon}`} alt="Szűrés" />
-          <span className={styles.tooltip}>Szűrés</span>
         </div>
+        <div className={styles.iconContainer}>
+          <img
+            src="/filter.svg"
+            className={`filter-button ${styles.icon}`}
+            alt="Szűrés"
+          />
+          <span className={styles.tooltip}>Szűrés</span>
         </div>
       </div>
     </header>
