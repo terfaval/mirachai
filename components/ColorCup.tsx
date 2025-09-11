ef29586c3874102ed490ec3d521ea14e540ccd41
terfaval <mate.fater@gmail.com>
2025-09-11 12:25:57 +0200

    added colorCup image centered behind chart

--- components/ColorCup.tsx
+++ components/ColorCup.tsx
@@ -10,17 +10,16 @@ export default function ColorCup({ teaName, color }: Props) {
   const cupColor = color ?? getTeaColor(teaName ?? '');
   return (
     <div className="relative w-32 h-32 mx-auto">
-      <img
-        src="/colorCup.png"
-        alt=""
-        className="absolute inset-0 w-full h-full object-contain"
-      />
-      <div className="absolute inset-0 rounded-full border-8 border-gray-200" />
       <div
         className="absolute inset-1 rounded-full"
         style={{
           background: `radial-gradient(circle at 50% 30%, ${cupColor}, ${cupColor}cc)`,
         }}
+      />
+      <img
+        src="/colorCup.png"
+        alt=""
+        className="absolute inset-0 w-full h-full object-contain pointer-events-none"
       />
     </div>
   );
