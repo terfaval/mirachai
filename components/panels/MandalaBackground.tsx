3141e8375b4f42389f18a69182a5accdbf85b2b4
terfaval <mate.fater@gmail.com>
2025-09-13 10:00:35 +0200

    fixed mandala design

--- components/panels/MandalaBackground.tsx
+++ components/panels/MandalaBackground.tsx
@@ -5,7 +5,11 @@ export default function MandalaBackground({ color = 'rgba(0,0,0,0.1)', category 
   const src = getMandalaPath(category);
   return (
     <div style={{ position:'absolute', inset:0, display:'grid', placeItems:'center', pointerEvents:'none' }}>
-      <img src={src} alt="" style={{ width:'72%', opacity:0.08, filter:`drop-shadow(0 0 0 ${color})` }} />
+      <img
+        src={src}
+        alt=""
+        style={{ width:'200%', maxWidth:'none', opacity:0.5, filter:`drop-shadow(0 0 0 ${color})` }}
+      />
     </div>
   );
 }
