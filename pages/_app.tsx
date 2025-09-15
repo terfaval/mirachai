b82f312c19524461d5efe0f8ade1d4ded1ecc910
terfaval <mate.fater@gmail.com>
2025-09-15 16:19:03 +0200

    general updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,18 +36,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato&family=Titan+One&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <Component {...pageProps} />
     </>
   );
