50183308da76e119dee75663fef91a46a4fe0a2e
terfaval <mate.fater@gmail.com>
2025-09-23 23:55:30 +0200

    added background image support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,45 @@
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 import { useEffect } from 'react';
 import '../styles/globals.css';
 import '@/styles/pager-global.css';
 
+const USE_TIME_BASED_BACKGROUND = false;
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    const updateBackground = () => {
-      const hour = new Date().getHours();
-      let suffix = '';
-      if (hour >= 5 && hour < 8) {
-        suffix = 'dawn';
-      } else if (hour >= 8 && hour < 11) {
-        suffix = 'morning';
-      } else if (hour >= 11 && hour < 14) {
-        suffix = 'noon';
-      } else if (hour >= 14 && hour < 17) {
-        suffix = 'afternoon';
-      } else if (hour >= 17 && hour < 20) {
-        suffix = 'evening';
-      } else {
-        suffix = 'night';
-      }
-      const url = `url('/background_${suffix}.png')`;
+    const setBackground = (url: string) => {
       const targets = [document.documentElement, document.body];
       targets.forEach((el) => {
         el.style.backgroundImage = url;
       });
     };
 
-    updateBackground();
-    const interval = setInterval(updateBackground, 60 * 60 * 1000);
-    return () => clearInterval(interval);
+    if (USE_TIME_BASED_BACKGROUND) {
+      const updateBackground = () => {
+        const hour = new Date().getHours();
+        let suffix = '';
+        if (hour >= 5 && hour < 8) {
+          suffix = 'dawn';
+        } else if (hour >= 8 && hour < 11) {
+          suffix = 'morning';
+        } else if (hour >= 11 && hour < 14) {
+          suffix = 'noon';
+        } else if (hour >= 14 && hour < 17) {
+          suffix = 'afternoon';
+        } else if (hour >= 17 && hour < 20) {
+          suffix = 'evening';
+        } else {
+          suffix = 'night';
+        }
+        setBackground(`url('/background_${suffix}.png')`);
+      };
+
+      updateBackground();
+      const interval = setInterval(updateBackground, 60 * 60 * 1000);
+      return () => clearInterval(interval);
+    }
+
+    setBackground("url('/background_backup.png')");
   }, []);
 
   return (
