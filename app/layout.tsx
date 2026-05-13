084c17e20ed77e7aa76d7bbf6cb4e77e7fa02548
terfaval <mate.fater@gmail.com>
2026-05-13 16:17:45 +0200

    added logo and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/mirachai_logo.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/mirachai_logo.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,8 @@ export default class MyDocument extends Document {
     return (
       <Html lang="hu">
         <Head>
+          <link rel="icon" href="/mirachai_logo.svg" type="image/svg+xml" />
+          <link rel="shortcut icon" href="/mirachai_logo.svg" type="image/svg+xml" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link href="https://fonts.googleapis.com/css2?family=Lato&family=Titan+One&display=swap" rel="stylesheet" />
